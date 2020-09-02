7281ce16fe5eec6c9b46544d46c50e4128b2f152
akash <akashar.rai@gmail.com>
2020-09-03 05:10:26 +0530

    UI

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const cells = document.querySelectorAll('.col');
+let turn = "x";
+
+function win(){
+    let winCondition = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,7]
+    ]
+}
+
+function onClick(clickedCellEvent){
+}
+
+function intialize{}
+
